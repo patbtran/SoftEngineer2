8e589c5ee28f5a8b0fee2755e8079f1f55face8e
Patricia T <ptran23@charlotte.edu>
2025-10-22 22:18:37 -0400

    Installed fastapi and uvicorn in main.py

--- Assignment4/main.py
+++ Assignment4/main.py
@@ -1,2 +1,12 @@
-print("Testing git")
-print("Lets try this again?")
+from typing import Union
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+@app.get("/items{item_id}")
+def read_item(item_id: int, q: Union[str,None] = None):
+    return {"item_id": item_id, "q": q}
